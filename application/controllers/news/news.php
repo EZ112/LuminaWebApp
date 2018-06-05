47d8b5a803edd1fb37e5df347642769df323be80
haiyee99 <HansenYoung0707@gmail.com>
2018-06-05 09:14:51 +0700

    News, History

--- application/controllers/news/news.php
+++ application/controllers/news/news.php
@@ -7,4 +7,24 @@ class News extends CI_Controller {
 	{
 		$this->load->view('news/news');
 	}
+
+	public function getNewsDetail(){
+		$arr = array(
+			'InNewsID' => $_POST['newsID']
+		);
+
+		$sp = "CALL sp_getNewsDetail(?)";
+		$result = $this->db->query($sp,$arr)->result();
+		echo json_encode($result);
+	}
+
+	public function getLatestNews(){
+		$arr = array(
+			'InNewsID' => $_POST['newsID']
+		);
+
+		$sp = "CALL sp_getLatestNews(?)";
+		$result = $this->db->query($sp, $arr)->result();
+		echo json_encode($result);
+	}
 }
--- application/views/news/news.php
+++ application/views/news/news.php
@@ -11,92 +11,21 @@
 		<div class="news_list grid-container">
 			<div>
 				<div class="title">Read</div>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
+				<a class="readNow">
+					<!-- <div class="date">21 Mar 2018</div>
 					<div class="list_content">
 						Adaptasi Anime "ISLAND" Ungkap Staf Produksinya
-					</div>
+					</div> -->
 				</a>
 			</div>
-			<div>
-				<div class="title">Popular News</div>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
-				<a href="#">
-					<div class="date">21 Mar 2018</div>
-					<div class="list_content">Lorem Ipsum atadatada</div>
-				</a>
+			<div class="latestNews">
+				<div class="title">Latest News</div>
 			</div>
 		</div>
 
 		<!--News Content-->
 		<div class="news_content_wrapper">
 			<div class="news_content">
-				<div class="img_container"></div>
-				<div class="tags">ANIME</div>
-				<div class="title">Adaptasi Anime ‘ISLAND’ Ungkap Staf Produksinya</div>
-				<div class="date">Kate Springer, Lumina • Published 21 Mar 2018</div> 
-				<div class="content">
-					<!-- <div class="img_container"><img id="fly" src="assets/image/island.jpg"></div> -->
-					Situs resmi dari adaptasi anime “ISLAND” telah mengungkap staf produksinya. Adaptasi VN ini direncanakan tayang pada tahun 2018 ini.
-					<br><br>
-					Staf produksinya adalah:
-					<br><br>
-					<li>Sutradara: Keiichiro Kawaguchi</li>
-					<li>Komposisi Seri: Naruhisa Arakawa</li>
-					<li>Desainer Karakter: Kousuke Kawamura</li>
-					<li>Musik: Akiyuki Tateyama</li>
-					<li>Lagu Pembuka: Yukari Tamura</li>
-					<li>Lagu Penutup: Asaka</li>
-					<li>Studio Produksi: feel.</li>
-					<br>
-					Seiyuu yang akan hadir beserta peran mereka adalah:
-					<br><br>
-					<li>Yukari Tamura sebagai <b>Rinne Ohara</b></li>
-					<li>Kana Asumi sebagai <b>Karen Kurutsu</b></li>
-					<li>Hibiku Yamamura <b>sebagai Sora Garandou</b></li>
-					<br>
-					VN in awalnya rilis pada April 2016 untuk PC dan rilis pada PlayStation Vita di 23 Februari
-					2017. Terdapat juga adaptasi manga karya Naoya Yao yang rilis pada April 2016.
-					<br><br>
-					Island menceritakan tentang pulau bernama Urashima. Sebuah insiden yang menimpa tiga keluarga besar pulau tersebut dari lima tahun yang lalu menyebabkan pulau tersebut terisolasi. Pulau tersebut terisolasi akibat penyakit lokal bernama Baimonbyou, tiga gadis dari pulau tersebut merupakan kunci untuk menyelamatkan pulau itu, namun tradisi membuat mereka tidak dapat bertindak. Suatu hari sebuah pria terdampar di pulau tersebut dan mengubah segalanya.
-					<br><br>
-					Gamenya diproduksi oleh Ryuichiro Yamakawa, Yousai Kuuchuu adalah ilustrator seri ini, sementara G.O. mengerjakan skenarionya.
-				</div>
 			</div>
 		</div>
 	</div>
